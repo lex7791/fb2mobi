e5b865fe29b874d827c5489962bae9aadf1ba4b0
rakunov <rakunov@60945433-3a4a-0410-813f-cbd0dfcc0603>
2008-04-07 14:34:58 +0000

    v.1.0

--- fb2mobi/Program.cs
+++ fb2mobi/Program.cs
@@ -13,66 +13,100 @@ namespace fb2mobi
         private static string filename;
         private static string opfxsl;
         private static string bodyxsl;
-        private static string basename;
+        private static string basepath;
 
         [STAThread]
         static void Main(string[] args)
         {
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]));
+            if(args.Length == 0){
+                Console.WriteLine("Usage: fb2mobi <file.fb2> [,<body.xsl> [,<opf.xsl>]]\n");
+                return;
+            }
+            else
+                filename = args[0];
+            if(!File.Exists(filename)){
+                Console.WriteLine("File: \"" + filename + "\" not found\n");
+                return;
+            }
+            bodyxsl = "FB2_2_xhtml.xsl";
+            if (args.Length > 1)
+                bodyxsl = args[1];
+            
+            opfxsl = "FB2_2_opf.xsl";
+            if (args.Length > 2)
+                opfxsl = args[2];
+
+            SXParser sp = new SXParser();
+
+            string rootpath = Path.GetDirectoryName(Path.GetFullPath(filename));
+            string basename = sp.getBookName(sp.transliteName(Path.GetFileName(filename)));
+            string bookname;
             try
             {
-                if(args.Length == 0){
-                    Console.WriteLine("Usage: fb2mobi <file.fb2> [,<body.xsl> [,<opf.xsl>]]\n");
-                    return;
-                }
-                else
-                    filename = args[0];
-                if(!File.Exists(filename)){
-                    Console.WriteLine("File: \"" + filename + "\" not found\n");
-                    return;
-                }
-                bodyxsl = "FB2_2_xhtml.xsl";
-                if (args.Length > 1)
-                    bodyxsl = args[1];
-                
-                opfxsl = "FB2_2_opf.xsl";
-                if (args.Length > 2)
-                    opfxsl = args[2];
+                bookname = sp.checkFile(rootpath, basename);
+                Directory.CreateDirectory(rootpath + "\\" + bookname);
+            }
+            catch (Exception )
+            {
+                rootpath = Path.GetTempPath();
+                bookname = sp.checkFile(rootpath, basename);
+                Directory.CreateDirectory(rootpath + "\\" + bookname);
+            }
+            basepath = rootpath + "\\" + bookname + "\\";
 
-                SXParser sp = new SXParser();
-                basename = sp.generateName(filename);
-
-                Console.WriteLine("Output: " + basename + "\n");
-                Directory.CreateDirectory(basename);
-
+            try
+            {
                 sp.saveImages();
-
-                string bookname = sp.getBookName() + ".opf";
-                sp.translate(opfxsl, bookname);
+                sp.translate(opfxsl, bookname+ ".opf");
                 sp.translate(bodyxsl, "index.html");
-
-                System.Diagnostics.Process process = new System.Diagnostics.Process();
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.RedirectStandardError = true;
-                process.StartInfo.CreateNoWindow = true;
-                process.StartInfo.FileName = "mobigen.exe";
-                process.StartInfo.Arguments = "-s0 -c2 -unicode -nomin " + basename + "\\" + bookname;
-                process.Start();
-
-                string str;
-                while ((str = process.StandardOutput.ReadLine()) != null)
-                {
-                    Console.WriteLine(str);
-                }
             }
             catch (XmlException e)
             {
                 Console.WriteLine("error occured: " + e.Message);
+                return;
+            }
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.FileName = "mobigen.exe";
+            process.StartInfo.Arguments = "-s0 -c2 -unicode -nomin \"" + basepath + bookname + ".opf\"";
+            process.Start();
+
+            string str;
+            while ((str = process.StandardOutput.ReadLine()) != null)
+                if(str.Length > 0)
+                    Console.WriteLine(str);
+
+            bookname += ".mobi";
+            if (File.Exists(basepath + bookname))
+            {
+                File.Move(basepath + bookname, rootpath + "\\" + bookname);
+                Console.WriteLine("Output: " + basepath);
             }
 
         }
 
-        string generateName(string file)
+        string checkFile(string dir, string file)
+        {
+            if (file.Length == 0)
+                file = "fb2mobi";
+
+            string name = dir + "\\" + file;
+            if (!File.Exists(name) && !Directory.Exists(name))
+                return file;
+
+            int cont = 1;
+            while (File.Exists(name + cont.ToString()) || Directory.Exists(name + cont.ToString()))
+                ++cont;
+            
+            return file + cont.ToString();
+        }
+
+        string transliteName(string file)
         {
             char[] rus = new char[] {
                     'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é'
@@ -94,34 +128,13 @@ namespace fb2mobi
                     name += lat[i];
                 else if(ch >= '0' && ch <= 127)
                     name += ch;
-            }
-            if (name.Length == 0 || File.Exists(name) || Directory.Exists(name))
-            {
-                int cont = 1;
-                while (File.Exists(name + cont.ToString()) || Directory.Exists(name + cont.ToString()))
-                    ++cont;
-                name += cont.ToString();
+                if (name.Length > 31)
+                    break;
             }
             return name;
         }
 
-        void saveImages()
-        {
-            XmlDocument dd = new XmlDocument();
-            dd.Load(filename);
-            XmlNode bin = dd["FictionBook"]["binary"];
-            while(bin != null)
-            {
-                FileStream fs = new FileStream(basename + "\\" + bin.Attributes["id"].InnerText, FileMode.Create);
-                BinaryWriter w = new BinaryWriter(fs);
-                w.Write(Convert.FromBase64String(bin.InnerText));
-                w.Close();
-                fs.Close();
-                bin = bin.NextSibling;
-            }
-        }
-
-        string getBookName()
+        string getBookName(string defname)
         {
             XmlDocument dd = new XmlDocument();
             dd.Load(filename);
@@ -133,10 +146,26 @@ namespace fb2mobi
                 {
                     string val = data.InnerText;
                     if (val.Length > 0)
-                        return generateName(val);
+                        return transliteName(val);
                 }
             }
-            return basename;
+            return defname;
+        }
+
+        void saveImages()
+        {
+            XmlDocument dd = new XmlDocument();
+            dd.Load(filename);
+            XmlNode bin = dd["FictionBook"]["binary"];
+            while (bin != null)
+            {
+                FileStream fs = new FileStream(basepath + bin.Attributes["id"].InnerText, FileMode.Create);
+                BinaryWriter w = new BinaryWriter(fs);
+                w.Write(Convert.FromBase64String(bin.InnerText));
+                w.Close();
+                fs.Close();
+                bin = bin.NextSibling;
+            }
         }
 
         void translate(string xsl, string name)
@@ -146,7 +175,7 @@ namespace fb2mobi
             XslCompiledTransform xslt = new XslCompiledTransform();
             xslt.Load(xsl);
 
-            XmlTextWriter writer = new XmlTextWriter(basename + "\\" + name, null);
+            XmlTextWriter writer = new XmlTextWriter(basepath + name, null);
             writer.Formatting = Formatting.Indented;
             
             xslt.Transform(reader, null, writer, null);
